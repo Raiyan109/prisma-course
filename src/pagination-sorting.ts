db3cfaa3dbea11cbc4f428865cf292c7f3c063d9
Raiyan109 <kraiyan109@gmail.com>
2025-07-09 07:56:46 +0600

    relation started

--- src/pagination-sorting.ts
+++ src/pagination-sorting.ts
@@ -3,7 +3,27 @@ import { PrismaClient } from "../generated/prisma";
 const prisma =new PrismaClient()
 
 const paginationSorting = async () => {
-    console.log('pagination');
+    // const offsetData = await prisma.post.findMany({
+    //     skip: 5,
+    //     take: 2
+    // })
+
+    // cursor based
+    const cursorbasedPagination = await prisma.post.findMany({
+        skip: 5,
+        take: 2,
+        cursor: {
+            id: 5
+        }
+    })
+
+    // Sorting
+    const sortedData = await prisma.post.findMany({
+        orderBy:{
+            title: 'asc'
+        }
+    })
+    console.log('cursor pagination data', sortedData);
     
 }
 
